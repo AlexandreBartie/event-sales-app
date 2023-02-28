d5f49e1d28b4ffe61238da96bf7bd7e6a147707f
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-02-28 04:51:45 -0300

    feat: show static sales in SalesList Panel

--- src/business/ESM-App/ESM-Services.ts
+++ src/business/ESM-App/ESM-Services.ts
@@ -2,11 +2,9 @@ import { DataBaseConnector } from '../../util/libDBConnector';
 import { mmoApp } from './ESM-Project';
 
 export class mmoServices {
-
   readonly DB: DataBaseConnector;
 
   constructor(app: mmoApp) {
     this.DB = new DataBaseConnector();
   }
-
 }
--- src/components/forms/AppForm/AppFormUX.tsx
+++ src/components/forms/AppForm/AppFormUX.tsx
@@ -6,16 +6,16 @@ import { MainForm } from '../MainForm/MainForm';
 import MainFormUX from '../MainForm/MainFormUX';
 import SplashFormUX from '../SplashForm/SplashFormUX';
 
-let main: MainForm;
+// let main: MainForm;
 
 export default function AppFormUX() {
   useEffect(() => {
     async function getMain() {
-      main = new MainForm(new mmoApp());
+      // main = new MainForm(new mmoApp());
     }
     getMain().catch(console.error);
   }, []);
 
-  if (true) return <MainFormUX ux={main} />;
+  if (true) return <MainFormUX ux={new MainForm(new mmoApp())} />;
   else return <SplashFormUX />;
 }
